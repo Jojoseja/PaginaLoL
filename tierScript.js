5cf4413f4f0a46cbe97ac0ee593358e8a5982174
jojoseja <jfeyjoo@gmail.com>
2025-08-06 17:45:20 +0200

    finished video

--- tierScript.js
+++ tierScript.js
@@ -1,8 +1,134 @@
 import { colors } from './colors.js'
 
 const settingsModal = document.querySelector(".settings-modal");
+const colorsContainer = settingsModal.querySelector(".colors");
+const tiersContainer = document.querySelector(".tiers");
 
-const colorsContainer = settingsModal.querySelector('.colors');
+let activeTier;
+
+const resetTierImages = (tier) => {
+    const images = tier.querySelectorAll('.items img');
+    images.forEach((img) => {
+        document.querySelector(".cards").appendChild(img);
+    });
+};
+
+const handleDeleteTier = () => {
+    if (activeTier) {
+        resetTierImages(activeTier);
+        activeTier.remove();
+        settingsModal.close();
+    }
+};
+
+const handleClearTier = () => {
+    if (activeTier) {
+        resetTierImages(activeTier);
+        settingsModal.close();
+    }
+};
+
+const handlePrependTier = () => {
+    if (activeTier){
+        tiersContainer.insertBefore(createTier(), activeTier);
+        settingsModal.close();
+    }
+};
+
+const handleAppendTier = () => {
+    if (activeTier){
+        tiersContainer.insertBefore(createTier(), activeTier.nextSibling);
+        settingsModal.close();
+    }
+};
+
+const handleSettignsClick = (tier) => {
+    activeTier = tier;
+
+    // populate the textarea
+    const label = tier.querySelector('.label');
+    settingsModal.querySelector('.tier-label').value = label.innerText;
+
+    // select the color
+    const color = getComputedStyle(label).getPropertyValue("--color");
+    settingsModal.querySelector(`input[value="${color}"]`).checked = true;
+
+    settingsModal.showModal();
+};
+
+const handleMoveTier = (tier, direction) => {
+    const sibling = direction === 'up' ? tier.previousElementSibling : tier.nextElementSibling;
+    if (sibling) {
+        const position = direction === 'up' ? 'beforebegin' : 'afterend';
+        sibling.insertAdjacentElement(position, tier);
+    }
+}
+
+
+const handleDragover = (event) => {
+    event.preventDefault(); //allow drop
+    const draggedImage = document.querySelector(".dragging");
+    const target = event.target;
+    
+    if (target.classList.contains('items')){
+        target.appendChild(draggedImage);
+    } else if (target.tagName === "IMG" && target !== draggedImage){
+        const {left, width} = target.getBoundingClientRect();
+        const midPoint = left + width / 2;
+
+        if (event.clientX < midPoint){
+            target.before(draggedImage)
+        } else {
+            target.after(draggedImage)
+        }
+    }
+};
+
+const handleDrop = (event) => {
+    event.preventDefault(); //allow default browser default
+};
+
+const createTier = (label = "Change me") => {
+
+    const tierColor = colors[tiersContainer.children.length % colors.length];
+
+    const tier = document.createElement("div");
+    
+    tier.className = "tier";
+    tier.innerHTML = `
+    <div class="label" contenteditable="plaintext-only" style="--color: ${tierColor}">
+        <span>${label}</span>
+    </div>
+    <div class="items"></div>
+    <div class="controls">
+        <button class="settings"><i class="bi bi-gear-fill"></i></button>
+        <button class="moveup"><i class="bi bi-chevron-up"></i></button>
+        <button class="movedown"><i class="bi bi-chevron-down"></i></button>
+    </div>`;
+
+    // Attach Event Listeners
+    tier
+    .querySelector('.settings')
+    .addEventListener("click", () => handleSettignsClick(tier));
+
+    tier
+    .querySelector('.moveup')
+    .addEventListener('click', () => handleMoveTier(tier, "up"));
+
+    tier
+    .querySelector('.movedown')
+    .addEventListener('click', () => handleMoveTier(tier, "down"));
+
+    tier
+    .querySelector(".items")
+    .addEventListener('dragover', handleDragover)
+
+    tier
+    .querySelector(".items")
+    .addEventListener('drop', handleDrop)
+
+    return tier;
+};  
 
 const initColorOptions = () => {
     colors.forEach((color) => {
@@ -13,4 +139,61 @@ const initColorOptions = () => {
     })
 }
 
+const initDefaultTierList = (params) => {
+    ['S', 'A', 'B', 'C', 'D', 'E', 'F'].forEach((label) =>{
+        tiersContainer.appendChild(createTier(label));
+    })
+}
+
+const initDraggables = () => {
+    const images = document.querySelectorAll('.cards img');
+    images.forEach((img) => {
+        img.draggable = true;
+
+        img.addEventListener('dragstart', () => img.classList.add("dragging"));
+        img.addEventListener('dragend', () => img.classList.remove("dragging"));
+    });
+}
+
+initDraggables();
+initDefaultTierList();
 initColorOptions();
+
+//* Event Listeners
+document.querySelector("h1").addEventListener("click", () => {
+  tiersContainer.appendChild(createTier());
+});
+
+
+settingsModal.addEventListener('click', (event) => {
+    if (event.target === settingsModal) {
+        settingsModal.close();
+    } else {
+        const action = event.target.id;
+        const actionMap = {
+            delete: handleDeleteTier,
+            clear: handleClearTier,
+            prepend: handlePrependTier,
+            append: handleAppendTier,
+        }
+        if (action && actionMap[action]){
+            actionMap[action]();
+        }
+    }
+});
+
+settingsModal.addEventListener('close', () => activeTier = null);
+
+settingsModal
+.querySelector('.tier-label')
+.addEventListener('input', (event) => {
+    if (activeTier) {
+        activeTier.querySelector(".label span").textContent = event.target.value;  
+    }
+});
+
+colorsContainer.addEventListener("change", (event) => {
+    if (activeTier) {
+        activeTier.querySelector(".label").style.setProperty("--color", event.target.value);
+    }
+});
